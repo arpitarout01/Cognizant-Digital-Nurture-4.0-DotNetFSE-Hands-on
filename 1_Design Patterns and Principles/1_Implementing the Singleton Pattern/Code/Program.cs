42096f18dd12c554a4f29beb3b16c3fa0a4226f8
Arpita Rout <arpitarout132@gmail.com>
2025-06-20 22:13:20 +0530

    Added Singleton Pattern implementation with code and output (removed accidental file)

---
+++ 1_Design Patterns and Principles/1_Implementing the Singleton Pattern/Code/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UploadService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Access Singleton instance
+            Singleton_pattern s1 = Singleton_pattern.Instance;
+            Singleton_pattern s2 = Singleton_pattern.Instance;
+
+            // Call a method
+            s1.ShowMessage();
+
+            // Check if both references point to the same object
+            Console.WriteLine("Are both instances same? " + Object.ReferenceEquals(s1, s2));
+
+            Console.ReadLine(); // Pause the console window
+        }
+    }
+}
---
+++ 1_Design Patterns and Principles/1_Implementing the Singleton Pattern/Code/Singleton_pattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadService
+{
+    public sealed class Singleton_pattern
+    {
+        private static Singleton_pattern instance = null;
+        private static object singletonLock = new object();        
+
+        private Singleton_pattern() 
+        { 
+            Console.WriteLine("Singleton instance created.");
+        }
+
+        public static Singleton_pattern Instance
+        {
+            get
+            {
+                lock(singletonLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton_pattern();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public void ShowMessage()
+        {
+           Console.WriteLine("Hello from Singleton!");
+        }
+    }
+}
