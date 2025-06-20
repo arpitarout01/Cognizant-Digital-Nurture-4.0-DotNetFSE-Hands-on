ba652492959eb06bdfcd5251bf27055b3162c18a
Arpita Rout <arpitarout132@gmail.com>
2025-06-21 00:12:25 +0530

    Added Factory Method Pattern folder with code and output

---
+++ 1_Design Patterns and Principles/2_Implementing the Factory Method Pattern/code/Continent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continental_App
+{
+    abstract class Continent
+    {
+        private List<Animal> _animals = new List<Animal>();
+
+        protected Continent()
+        {
+            CreateContinent();
+        }
+
+        public List<Animal> Animals => _animals;
+
+        public abstract void CreateContinent();
+    }
+}
---
+++ 1_Design Patterns and Principles/2_Implementing the Factory Method Pattern/code/Program.cs
@@ -0,0 +1,30 @@
+﻿using Continental_App.Continents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continental_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Continent[] continents = new Continent[]
+            {
+               new Africa(),
+               new Asia()
+            };
+            foreach (Continent continent in continents)
+            {
+                Console.WriteLine(continent.GetType().Name + ":");
+                foreach (Animal animal in continent.Animals)
+                {
+                    Console.WriteLine(" " + animal.GetType().Name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
