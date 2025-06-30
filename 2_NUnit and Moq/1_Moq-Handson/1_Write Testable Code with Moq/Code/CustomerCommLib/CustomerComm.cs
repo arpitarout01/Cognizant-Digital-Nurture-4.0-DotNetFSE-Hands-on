2c10ec6cda241aa9f58825d54624baa9763af3d9
arpitarout01 <arpitarout132@gmail.com>
2025-06-30 12:26:00 +0530

    Initial commit

---
+++ 2_NUnit and Moq/1_Moq-Handson/1_Write Testable Code with Moq/Code/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        IMailSender _mailSender;
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            // Define message and email address
+            return _mailSender.SendMail("arpitarout132@gmail.com", "Hello There !");
+        }
+    }
+}
